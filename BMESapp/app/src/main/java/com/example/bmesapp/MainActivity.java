e3c3b0294dfa54f6a75756ab2a3950d8e3b41e11
JPG0mez <jpgp70@protonmail.com>
2021-02-26 13:42:22 -0600

    updated ui and calendar

--- BMESapp/app/src/main/java/com/example/bmesapp/MainActivity.java
+++ BMESapp/app/src/main/java/com/example/bmesapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,4 +28,21 @@ public class MainActivity extends AppCompatActivity {
         eventViewer.setAdapter(myAdapter);
         eventViewer.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    public void openWindowmenu(View v) {
+
+        setContentView(R.layout.windowmenu);
+    }
+    public void backtomenu(View v) {
+        setContentView(R.layout.activity_main);
+        eventViewer = findViewById(R.id.eventViewer);
+
+        s1 = getResources().getStringArray(R.array.picture_names);
+        s2 = getResources().getStringArray(R.array.description);
+
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this, s1, s2, images);
+        eventViewer.setAdapter(myAdapter);
+        eventViewer.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
