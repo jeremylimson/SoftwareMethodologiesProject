d2a60b00c06f37676b8d959dbc4c772d45a4d6ee
jburkert33 <78981010+jburkert33@users.noreply.github.com>
2021-04-21 13:18:03 -0500

    CalanderView delete

--- BMESapp/app/src/main/java/com/example/navdrawer/ui/calendar/CalendarFragment.java
+++ BMESapp/app/src/main/java/com/example/navdrawer/ui/calendar/CalendarFragment.java
@@ -16,20 +16,14 @@ import com.example.navdrawer.R;
 
 public class CalendarFragment extends Fragment {
 
-    private CalendarViewModel calendarViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        calendarViewModel =
-                new ViewModelProvider(this).get(CalendarViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_calendar, container, false);
-        final TextView textView = root.findViewById(R.id.text_calendar);
-        calendarViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+
+        
+
+
+        return v;
     }
 }
